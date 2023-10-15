4b9eee88275ce5819dbeb2b2e5ef9445ba2c464d
MKE <mkaanerkoc@gmail.com>
2023-10-15 19:17:58 +0200

    test added

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,3 +1,18 @@
-def test_setup():
-    assert 1 == 1
+import unittest
 
+from fast_binance import OnlinePriceFetcher
+
+
+class TestFunctionality(unittest.TestCase):
+    def test_setup(self):
+        pf = OnlinePriceFetcher('spot')
+        res = pf.download(['SEIUSDT', 'FTTUSDT'], 
+                          interval='1h', 
+                          start_str='2023-01-01',
+                          end_str='2023-08-01' )
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
